bef3a0809d1b848b5947f13bf565a7bd02ab758d
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-05 14:14:02 +0100

    .

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import {
+	BaseEntity,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from "typeorm";
+
+@Entity() // db table
+export class User extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column({ unique: true })
+	username!: string;
+
+	@Column({ unique: true })
+	email!: string;
+
+	@Column()
+	password!: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello Backend");
+require("dotenv").config();
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import express, { Application } from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+	const AppDataSource = new DataSource({
+		type: "postgres",
+		database: "blocdash",
+		username: process.env.DB_USERNAME_DEV,
+		password: process.env.DB_PASSWORD_DEV,
+		logging: true,
+		synchronize: true,
+		entities: [User],
+	});
+	AppDataSource.initialize()
+		.then(() => {
+			console.log("Database connected :", AppDataSource.options.database);
+		})
+		.catch((error) => console.log("Database", error));
+
+	const app: Application = express();
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+		// context: ({req, res} : Context ) => ({req, res})
+		plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+	});
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app: app as any, cors: false });
+
+	app.listen(4000, () => console.log("Server started on port 4000"));
+};
+
+main().catch((error) => console.log(error));
