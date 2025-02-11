8babbb6aad9bcb59c3e4e86e2e155196cd4b65de
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-12 00:30:35 +0100

    .

--- src/db/dataSourceProd.ts
+++ src/db/dataSourceProd.ts
@@ -6,21 +6,11 @@ dotenv.config();
 let connectionOptions: DataSourceOptions = {
 	type: "postgres",
 	url: process.env.DB_URL,
-	// host: process.env.DB_HOST,
-	// port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-	// username: process.env.DB_USERNAME, // postgre username
-	// password: process.env.DB_PASSWORD, // postgre password
-	// database: process.env.DB_DATABASE, // postgre db, needs to be created before
 	synchronize: false,
 	dropSchema: false,
 	logging: true,
-	// logger: "file",
-	entities: ["dist/entities/*.js"],
-	migrations: ["dist/migrations/*.js"],
-	// migrations: ["src/migrations/**/*.ts"],
-	// migrationsRun: true,
-	//   subscribers: ['src/subscriber/**/*.ts'],
-	//   migrationsTableName: 'migration_table',
+	entities: ["src/entities/**/*.ts"],
+	migrations: ["src/migrations/**/*.ts"],
 };
 
 export default new DataSource({
