e56375ef9e1844d1e20f6de592bdbc376e01186c
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-02-12 16:05:14 +0100

    disable cron.schedule

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ import cors from "cors";
 import express, { Application } from "express";
 import session from "express-session";
 import mongoose from "mongoose";
-import cron from "node-cron";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { __prod__, COOKIE_NAME } from "./constants";
 import AppDataSource from "./db/dataSourceProd";
 import { UserResolver } from "./resolvers/user";
-import { fetchAndStoreTransactions } from "./utils/fetchAndStoreTransactions";
 
 const app: Application = express();
 
@@ -78,7 +76,7 @@ const main = async () => {
 
 	//
 	// Schedule the heartbeat function to run every hour
-	cron.schedule("* * * * *", fetchAndStoreTransactions);
+	// cron.schedule("* * * * *", fetchAndStoreTransactions);
 };
 
 // async function connectWithRetry() {
