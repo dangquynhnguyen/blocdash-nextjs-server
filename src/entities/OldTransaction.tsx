224e5094b679f4624b48c5ee94e46a65ed9dced6
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-03-05 15:08:55 +0100

    .....

--- src/entities/OldTransaction.tsx
+++ src/entities/OldTransaction.tsx
@@ -1,10 +1,8 @@
 import "reflect-metadata";
 import { ObjectType } from "type-graphql";
-import { Entity, Index } from "typeorm";
+import { Entity } from "typeorm";
 import { Transaction } from "./Transaction";
 
 @ObjectType()
-@Index("idx_to_created_at", ["to_account_identifier", "created_at"])
-@Index("idx_from_created_at", ["from_account_identifier", "created_at"])
 @Entity()
 export class OldTransaction extends Transaction {}
