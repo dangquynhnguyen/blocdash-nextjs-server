f0c4e0323c63a3547cee1dccfb0037209c083c55
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-03-18 12:56:33 +0100

    ..

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ import AppDataSource from "./db/dataSourceProd";
 import { UserResolver } from "./resolvers/user";
 // import { calculateUniqueWalletStats } from "./utils/calculateUniqueWalletStats";
 import { calculateUniqueWalletStats } from "./utils/calculateUniqueWalletStats";
-import { fetchAndStoreTransactions } from "./utils/fetchAndStoreTransactions";
-import { updateAccountBalances } from "./utils/updateAccountHourlyBalance";
 
 require("dotenv").config();
 
@@ -91,8 +89,8 @@ const main = async () => {
 	);
 
 	// Schedule the heartbeat function to run every minute
-	cron.schedule("40 * * * * *", fetchAndStoreTransactions);
-	cron.schedule("* * * * *", updateAccountBalances);
+	// cron.schedule("40 * * * * *", fetchAndStoreTransactions);
+	// cron.schedule("* * * * *", updateAccountBalances);
 
 	// Run immediately on server start
 	// calculateUniqueWalletStats();
