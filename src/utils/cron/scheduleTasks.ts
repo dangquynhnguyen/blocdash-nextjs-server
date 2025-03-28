49cfa580d5ba538c1579a6de34d79f3280413b22
dangquynhnguyen <nguyendangquynh94@gmail.com>
2025-03-28 17:36:08 +0100

    Update setupCronJobs

--- src/utils/cron/scheduleTasks.ts
+++ src/utils/cron/scheduleTasks.ts
@@ -4,14 +4,14 @@ import { calculateUniqueWalletStats } from "../db/uniqueWallets";
 import { updateAccountBalances } from "../db/updateAccountBalance";
 
 export function setupCronJobs(): void {
-	// Fetch and store transactions every minute at 40s
-	cron.schedule("40 * * * * *", fetchAndStoreTransactions);
+	// Fetch and store transactions every minute at 30s
+	cron.schedule("30 * * * * *", fetchAndStoreTransactions);
 
 	// Update account balances every minute
 	cron.schedule("* * * * *", updateAccountBalances);
 
-	// Calculate unique wallet stats every minute at 20s
-	cron.schedule("20 * * * * *", calculateUniqueWalletStats);
+	// Calculate unique wallet stats every minute at 30 minutes
+	cron.schedule("20 */30 * * * *", calculateUniqueWalletStats);
 
 	console.log("Cron jobs scheduled successfully");
 }
